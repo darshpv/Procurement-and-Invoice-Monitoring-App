a3da8d8a1a16707b00e5e6a49fc0afd6123c6ce7
Darsh Chaudhary <darsh.chaudhary@predusk.local>
2026-06-26 15:31:23 +0530

    Top companies now displaying on frontend

--- frontend/src/components/TopCompaniesTable.tsx
+++ frontend/src/components/TopCompaniesTable.tsx
@@ -1,24 +1,78 @@
 import { ENDPOINTS, instance } from "../utils/api"
 import { SortingCategories } from "../enums";
-//import type { SortingCategories } from "../types/sortingCategories";
+import type { ComparisonData } from "../types/ComparisonData";
+import { useState, useEffect } from "react";
+import "./TopCompaniesTable.css"
 
 export default function TopCompaniesTable() {
+
+    const [displayedCompanies, setDisplayedCompanies] = useState<ComparisonData[]>();
+    const [sortingCategory, setSortingCategory] = useState<string>(SortingCategories.pending_invoice_qty);
+ 
     async function getTopCompanies(category: string) {
-        console.log(category);
         try {
             const response = await instance.get(ENDPOINTS.GET_COMPARISON_DATA(category));
-            console.log(response);
+            if (response.data != null) {
+                setDisplayedCompanies(response.data);
+            }
         } catch(e) {
             console.log(e);
         };
     };
+
+    useEffect(() => {
+        getTopCompanies(sortingCategory)
+    }, [sortingCategory])
     return (
-        <div>
-            <button
-                onClick={() => getTopCompanies(SortingCategories.delayed_deliveries_count)}
-            >
-                Click Me
-            </button>
+        <div className="top-companies-table">
+            <h2>
+                Top 10 Companies
+            </h2>
+            <select
+                    value={sortingCategory}
+                    onChange={(e) => {
+                        setSortingCategory(e.target.value)
+                    }}
+                    title="Edit Sorting Category"
+                    className="sorting-category-select"
+                >
+                    <option value="total_po_value">Total PO Value</option>
+                    <option value="pending_invoice_value">Pending Invoice Value</option>
+                    <option value="pending_invoice_qty">Pending Quantity</option>
+                    <option value="delayed_deliveries_count">Delayed Deliveries Count</option>
+                    <option value="payments_pending_count">Payments Pending Count</option>
+                </select>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Company Name</th>
+                        <th>Total PO Value</th>
+                        <th>Total Invoice Value</th>
+                        <th>Pending Invoice Value</th>
+                        <th>Pending Quantity</th>
+                        <th>Delayed Deliveries Count</th>
+                        <th>Payments Pending Count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {displayedCompanies?.map(
+                        (company) => (
+                            <tr>
+                                <td>{company.company_name}</td>
+                                <td>INR {(company.total_po_value/10000000).toFixed(2)} Cr</td>
+                                <td>INR {(company.total_invoice_value/10000000).toFixed(2)} Cr</td>
+                                <td>INR {(company.pending_invoice_value/10000000).toFixed(2)} Cr</td>
+                                <td>{(company.pending_invoice_qty).toLocaleString("en-IN")}</td>
+                                <td>{company.delayed_deliveries_count}</td>
+                                <td>{company.payments_pending_count}</td>
+                            </tr>
+                        )
+                    )}
+                </tbody>
+                
+            </table>
+            
         </div>
     )
 }
+
--- frontend/src/types/sortingCategories.ts
+++
@@ -1,9 +0,0 @@
-// export type SortingCategories = {
-
-//   total_po_value: "total_po_value",
-//   pending_invoice_value: "pending_invoice_value",
-//   pending_invoice_qty: "pending_invoice_qty",
-//   delayed_deliveries_count: "delayed_deliveries_count",
-//   payments_pending_count: "payments_pending_count",
-
-// };
